c7f21d31c07fe2e9d698cd1aa0ed407b2224dd0f
hiki8man <hiki8man@users.noreply.github.com>
2026-09-17 04:25:00 +0000

    deploy: 3972f132260b8bb07da76ce5c476fe635029fd9b

---
+++ _astro/permalinkConfig.BUdZyyqa.js
@@ -0,0 +1,1 @@
+import{n as a}from"./config-overlay.DTQ08Q4F.js";var n=a("permalink",{enable:!1,format:"%postname%"});
---
+++ _astro/postListConfig.C9i_4yTD.js
@@ -0,0 +1,1 @@
+import{n as t}from"./config-overlay.DTQ08Q4F.js";var o=t("postList",{pageSize:8,layout:{mode:"list",cover:"right",cardWidth:"regular"}});export{o as t};
